1d4bf09ed20bd96b450db40694c6f417564924b9
Andrew Zody <zodyac@protonmail.com>
2023-06-06 15:42:20 -0400

    Adding MT5 Data Import

---
+++ DataImport.py
@@ -0,0 +1,77 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  6 14:36:35 2023
+
+@author: zodyac
+"""
+
+import datetime #  Tools for manipulating dates and times
+import pytz # Cross platform time zone calculations
+import pandas as pd # Data analysis Library
+import MetaTrader5 as mt5 # Metatrader5 api
+import numpy as np # Data analysis library
+
+# Create Universe of time frames
+frame_M15 = mt5.TIMEFRAME_M15 # 15-minute time frame
+frame_M30 = mt5.TIMEFRAME_M30 # 30-minute time frame
+frame_H1 = mt5.TIMEFRAME_H1 # Hourly time frame
+frame_H4 = mt5.TIMEFRAME_H4 # 4-hour time frame 
+frame_D1 = mt5.TIMEFRAME_D1 # Day time frame 
+frame_W1 = mt5.TIMEFRAME_W1 # Week time frame 
+frame_MN1 = mt5.TIMEFRAME_MN1 # Month time frame 
+
+now = datetime.datetime.now()
+
+# Asset Classes
+# Currencies a.k.a. forex - bigget financial market in terms of daily volume
+## EURUSD pair refers to price of 1 EUR in terms of USD
+
+# Cryptocurrencies
+## BTCUSD refers to p
+
+# Commodities
+## Physical Assets like gold, silver, coppoer
+
+# Equity Indices
+# SP500 for US and FTSE 100 for UK
+assets = ['EURUSD', 'USDCHF', 'GBPUSD', 'USCAD', 'BTCUSD', 'ETHUSD', 'XAUUSD', 'XAGUSD', 'SP500m', 'UK100']
+
+
+def get_quotes(time_frame, year = 2005, month = 1, day = 1, asset = "EURUSD") :
+    if not mt5.initialize() :
+        print("initialize() failed, error code = ", mt5.last_error())
+        quit()
+    
+    timezone = pytz.timezone("America/New_York")
+    time_from = datetime.datetime(year, month, day, tzinfo = timezone)
+    time_to = datetime.datetime.now(timezone) + datetime.timedelta(days=1)
+    rates = mt5.copy_rates_range(asset, time_frame, time_from, time_to)
+    rates_frame = pd.DataFrame(rates)
+    return rates_frame
+
+
+def mass_import(asset, time_frame) :
+    if time_frame == 'H1' :
+        data = get_quotes(frame_H1, 2023, 1, 1, asset = assets[asset])
+        data = data.iloc[:, 1:5].values
+        data = data.round(decimals = 5)
+        
+    if time_frame == 'D1' :
+        data = get_quotes(frame_D1, 2023, 1, 1, asset = assets[asset])
+        data = data.round(decimals = 5)
+        
+    return data
+
+
+# import ETHUSD data
+my_data = mass_import(5, 'D1')
+
+my_data_np = np.array(my_data)
+
+# import GBPUSD data
+my_data = mass_import(2, 'H1')
+
+my_data_np = np.array(my_data)
+
+
+
---
+++ ReadMe.md
@@ -0,0 +1,24 @@
+# Getting Started
+## Required Installation
+1. Anaconda
+2. Spyder
+3. MetaTrader 5
+
+## Account Creation
+Open MetaTrader5 and create a free demo accont
+
+## Download MetaTrader 5 Dependencies
+> pip install Metatrader5
+> pip install pandas
+
+
+# Core Concepts
+## 4 key algorithm types
+1. Import Algorithm 
+- Deals with importing and preparing historical OHLC data to be analyzed or backtested
+2. Signal ALgorithm
+- Responsible for generating buy and sell orders
+3. Charting Algorithm
+- Chart the signals on the price chart 
+4. Performance Algorithm 
+- Calculate and analyze the resuls aquired from the signal algorithm thats a good step
