d1c84d3a61995b55df0bfd28c55642aa28df4c41
Andrew Zody <zodyac@protonmail.com>
2023-06-27 08:09:13 -0400

    Marubozu Pattern

--- data_import.py
+++ data_import.py
@@ -12,13 +12,15 @@ import MetaTrader5 as mt5 # Metatrader5 api
 import numpy as np # Data analysis library
 
 # Create Universe of time frames
+frame_M1 = mt5.TIMEFRAME_M1 # 1-minute time frame
+frame_M5 = mt5.TIMEFRAME_M5 # 1-minute time frame
 frame_M15 = mt5.TIMEFRAME_M15 # 15-minute time frame
 frame_M30 = mt5.TIMEFRAME_M30 # 30-minute time frame
 frame_H1 = mt5.TIMEFRAME_H1 # Hourly time frame
-frame_H4 = mt5.TIMEFRAME_H4 # 4-hour time frame 
-frame_D1 = mt5.TIMEFRAME_D1 # Day time frame 
-frame_W1 = mt5.TIMEFRAME_W1 # Week time frame 
-frame_MN1 = mt5.TIMEFRAME_MN1 # Month time frame 
+frame_H4 = mt5.TIMEFRAME_H4 # 4-hour time frame
+frame_D1 = mt5.TIMEFRAME_D1 # Day time frame
+frame_W1 = mt5.TIMEFRAME_W1 # Week time frame
+frame_MN1 = mt5.TIMEFRAME_MN1 # Month time frame
 
 now = datetime.datetime.now()
 
@@ -41,7 +43,7 @@ def get_quotes(time_frame, year = 2005, month = 1, day = 1, asset = "EURUSD") :
     if not mt5.initialize() :
         print("initialize() failed, error code = ", mt5.last_error())
         quit()
-    
+
     timezone = pytz.timezone("America/New_York")
     time_from = datetime.datetime(year, month, day, tzinfo = timezone)
     time_to = datetime.datetime.now(timezone) + datetime.timedelta(days=1)
@@ -55,16 +57,26 @@ def mass_import(asset, time_frame) :
         data = get_quotes(frame_H1, 2023, 1, 1, asset = assets[asset])
         data = data.iloc[:, 1:5].values
         data = data.round(decimals = 5)
-        
+
     if time_frame == 'D1' :
         data = get_quotes(frame_D1, 2023, 1, 1, asset = assets[asset])
+        data = data.iloc[:, 1:5].values
         data = data.round(decimals = 5)
-        
+
+    if time_frame == 'M1' :
+        data = get_quotes(frame_M1, 2023, 6, 10, asset = assets[asset])
+        data = data.iloc[:, 1:5].values
+        data = data.round(decimals = 5)
+    if time_frame == 'M5' :
+        data = get_quotes(frame_M5, 2023, 6, 20, asset = assets[asset])
+        data = data.iloc[:, 1:5].values
+        data = data.round(decimals = 5)
+
     return data
 
 
 # import ETHUSD data
-my_data = mass_import(5, 'D1')
+my_data = mass_import(5, 'M1')
 
 my_data_np = np.array(my_data)
 
