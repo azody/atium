02d114394c13715beb533c1495523e1d0e58fd52
Andrew Zody <zodyac@protonmail.com>
2024-08-21 17:04:54 -0400

    Refactor Double Troble

--- tester.py
+++ tester.py
@@ -6,7 +6,8 @@ from array_util import add_column
 from chart_util import signal_chart
 from performance import performance
 from rounding_util import rounding
-from pattern.trend_following.quintuplets import signal
+from pattern.trend_following.double_trouble import signal
+from volatility import average_true_range
 
 # Choose an Asset
 pair = 0 # EURUSD
@@ -21,12 +22,12 @@ my_data = mass_import(pair, horizon)
 my_data = add_column(my_data, 5)
 
 # Calling the Signal Function
-my_data = signal(my_data, 0, 3, 4, 5, 0.0000001)
-#my_data = rounding(my_data, 5)
 
-#my_data = signal(my_data, 0, 1, 2, 3, 4, 5)
+#my_data = rounding(my_data, 5)
+my_data = average_true_range(my_data, 10, 1, 2, 3, 4)
+my_data = signal(my_data, 0, 1, 2, 3, 4, 5, 6)
 # Charting the latest 150 Signals
 signal_chart(my_data, 0, 4, 5, window = 200)
 
 # Get Performance Metrics
-performance(my_data, 0, 4, 5, 6, 7, 8)
+my_data = performance(my_data, 0, 5, 6, 7, 8, 9)
