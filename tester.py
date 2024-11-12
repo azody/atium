0107e5658361d3477ceaab73e39dda1286be15ce
Andrew Zody <zodyac@protonmail.com>
2024-11-12 09:28:05 -0500

    Barrier  Refactor

--- tester.py
+++ tester.py
@@ -6,7 +6,7 @@ from array_util import add_column
 from chart_util import signal_chart
 from performance import performance
 from rounding_util import rounding
-from pattern.contrarian.doji import signal
+from pattern.contrarian.barrier import signal
 from volatility import average_true_range
 
 # Choose an Asset
@@ -23,9 +23,9 @@ my_data = add_column(my_data, 5)
 
 # Calling the Signal Function
 
-#my_data = rounding(my_data, 5)
+my_data = rounding(my_data, 4)
 #my_data = average_true_range(my_data, 10, 1, 2, 3, 4)
-my_data = signal(my_data, 0, 3, 4, 5)
+my_data = signal(my_data, 0, 1, 2, 3, 4, 5)
 # Charting the latest 150 Signals
 signal_chart(my_data, 0, 4, 5, window = 200)
 
