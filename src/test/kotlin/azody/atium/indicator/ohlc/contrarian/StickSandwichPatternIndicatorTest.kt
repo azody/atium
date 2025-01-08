9b94a39a69199c95395e73f6eee587039c4e63c3
azody <zodyac@protonmail.com>
2025-01-08 11:14:28 -0500

    Testing Multi-Git

---
+++ src/test/kotlin/azody/atium/indicator/ohlc/contrarian/StickSandwichPatternIndicatorTest.kt
@@ -0,0 +1,9 @@
+package azody.atium.indicator.ohlc.contrarian
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+class StickSandwichPatternIndicatorTest : FunSpec({
+
+
+})
