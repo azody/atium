985dce2c4f424bfb553088f94e69adce49bbb8c9
azody <zodyac@protonmail.com>
2025-01-10 11:38:28 -0500

    Buy Sell Accounting Functionality

--- src/test/kotlin/azody/atium/accounting/AccountingTest.kt
+++ src/test/kotlin/azody/atium/accounting/AccountingTest.kt
@@ -111,4 +111,271 @@ class AccountingTest :
             xPosition.price shouldBe BigDecimal(200)
             xPosition.quantity shouldBe BigDecimal(1)
         }
+
+        test("AddTrade - Sell on Empty Portfolio") {
+            val startingPortfolio =
+                Portfolio(
+                    positions = listOf(),
+                    cashPosition =
+                        CashPosition(
+                            instrument = "USD",
+                            quantity = BigDecimal(1_000),
+                        ),
+                )
+
+            val newTrade =
+                Trade(
+                    businessTime = 1,
+                    instrument = "AAPL",
+                    counterInstrument = "USD",
+                    settlementInstrument = "USD",
+                    price = BigDecimal(100),
+                    quantity = BigDecimal(2),
+                    type = TradeType.SELL,
+                    tradeSubType = TradeSubType.NONE,
+                    direction = Direction.LONG,
+                )
+
+            val resultingPortfolio = Accounting.addTrade(trade = newTrade, portfolio = startingPortfolio)
+
+            resultingPortfolio.cashPosition.quantity shouldBe BigDecimal(1_200)
+            resultingPortfolio.cashPosition.instrument shouldBe "USD"
+
+            val applPosition = resultingPortfolio.positions.filter { it.instrument == "AAPL" }[0]!!
+
+            applPosition.instrument shouldBe "AAPL"
+            applPosition.counterInstrument shouldBe "USD"
+            applPosition.lots.size shouldBe 1
+            applPosition.price shouldBe BigDecimal(100)
+            applPosition.quantity shouldBe BigDecimal(-2)
+        }
+
+        test("AddTrade - Sell on Portfolio - FIFO Full Lot") {
+            val lots =
+                listOf(
+                    Lot(
+                        instrument = "AAPL",
+                        counterInstrument = "USD",
+                        price = BigDecimal(100),
+                        quantity = BigDecimal(3),
+                        acquisitionTime = 1,
+                        direction = Direction.LONG,
+                    ),
+                    Lot(
+                        instrument = "AAPL",
+                        counterInstrument = "USD",
+                        price = BigDecimal(200),
+                        quantity = BigDecimal(1),
+                        acquisitionTime = 2,
+                        direction = Direction.LONG,
+                    ),
+                )
+
+            val startingPortfolio =
+                Portfolio(
+                    positions = lots.toPositions(),
+                    cashPosition =
+                        CashPosition(
+                            instrument = "USD",
+                            quantity = BigDecimal(1_000),
+                        ),
+                )
+
+            val newTrade =
+                Trade(
+                    businessTime = 1,
+                    instrument = "AAPL",
+                    counterInstrument = "USD",
+                    settlementInstrument = "USD",
+                    price = BigDecimal(100),
+                    quantity = BigDecimal(3),
+                    type = TradeType.SELL,
+                    tradeSubType = TradeSubType.NONE,
+                    direction = Direction.LONG,
+                )
+
+            val resultingPortfolio = Accounting.addTrade(trade = newTrade, portfolio = startingPortfolio)
+
+            resultingPortfolio.cashPosition.quantity shouldBe BigDecimal(1_300)
+            resultingPortfolio.cashPosition.instrument shouldBe "USD"
+
+            val applPosition = resultingPortfolio.positions.filter { it.instrument == "AAPL" }[0]!!
+
+            applPosition.instrument shouldBe "AAPL"
+            applPosition.counterInstrument shouldBe "USD"
+            applPosition.price shouldBe BigDecimal(200)
+            applPosition.quantity shouldBe BigDecimal(1)
+        }
+
+        test("AddTrade - Sell on Portfolio - FIFO Partial Lot") {
+            val lots =
+                listOf(
+                    Lot(
+                        instrument = "AAPL",
+                        counterInstrument = "USD",
+                        price = BigDecimal(100),
+                        quantity = BigDecimal(3),
+                        acquisitionTime = 1,
+                        direction = Direction.LONG,
+                    ),
+                    Lot(
+                        instrument = "AAPL",
+                        counterInstrument = "USD",
+                        price = BigDecimal(200),
+                        quantity = BigDecimal(1),
+                        acquisitionTime = 2,
+                        direction = Direction.LONG,
+                    ),
+                )
+
+            val startingPortfolio =
+                Portfolio(
+                    positions = lots.toPositions(),
+                    cashPosition =
+                        CashPosition(
+                            instrument = "USD",
+                            quantity = BigDecimal(1_000),
+                        ),
+                )
+
+            val newTrade =
+                Trade(
+                    businessTime = 1,
+                    instrument = "AAPL",
+                    counterInstrument = "USD",
+                    settlementInstrument = "USD",
+                    price = BigDecimal(100),
+                    quantity = BigDecimal(2),
+                    type = TradeType.SELL,
+                    tradeSubType = TradeSubType.NONE,
+                    direction = Direction.LONG,
+                )
+
+            val resultingPortfolio = Accounting.addTrade(trade = newTrade, portfolio = startingPortfolio)
+
+            resultingPortfolio.cashPosition.quantity shouldBe BigDecimal(1_200)
+            resultingPortfolio.cashPosition.instrument shouldBe "USD"
+
+            val applPosition = resultingPortfolio.positions.filter { it.instrument == "AAPL" }[0]!!
+
+            applPosition.instrument shouldBe "AAPL"
+            applPosition.counterInstrument shouldBe "USD"
+            applPosition.price shouldBe BigDecimal(150)
+            applPosition.quantity shouldBe BigDecimal(2)
+        }
+
+        test("AddTrade - Sell on Portfolio - LIFO Full Lot") {
+            val lots =
+                listOf(
+                    Lot(
+                        instrument = "AAPL",
+                        counterInstrument = "USD",
+                        price = BigDecimal(100),
+                        quantity = BigDecimal(3),
+                        acquisitionTime = 1,
+                        direction = Direction.LONG,
+                    ),
+                    Lot(
+                        instrument = "AAPL",
+                        counterInstrument = "USD",
+                        price = BigDecimal(200),
+                        quantity = BigDecimal(1),
+                        acquisitionTime = 2,
+                        direction = Direction.LONG,
+                    ),
+                )
+
+            val startingPortfolio =
+                Portfolio(
+                    positions = lots.toPositions(),
+                    cashPosition =
+                        CashPosition(
+                            instrument = "USD",
+                            quantity = BigDecimal(1_000),
+                        ),
+                    sellMethodology = SellMethodology.LIFO,
+                )
+
+            val newTrade =
+                Trade(
+                    businessTime = 1,
+                    instrument = "AAPL",
+                    counterInstrument = "USD",
+                    settlementInstrument = "USD",
+                    price = BigDecimal(100),
+                    quantity = BigDecimal(1),
+                    type = TradeType.SELL,
+                    tradeSubType = TradeSubType.NONE,
+                    direction = Direction.LONG,
+                )
+
+            val resultingPortfolio = Accounting.addTrade(trade = newTrade, portfolio = startingPortfolio)
+
+            resultingPortfolio.cashPosition.quantity shouldBe BigDecimal(1_100)
+            resultingPortfolio.cashPosition.instrument shouldBe "USD"
+
+            val applPosition = resultingPortfolio.positions.filter { it.instrument == "AAPL" }[0]!!
+
+            applPosition.instrument shouldBe "AAPL"
+            applPosition.counterInstrument shouldBe "USD"
+            applPosition.price shouldBe BigDecimal(100)
+            applPosition.quantity shouldBe BigDecimal(3)
+        }
+
+        test("AddTrade - Sell on Portfolio - Oversell") {
+            val lots =
+                listOf(
+                    Lot(
+                        instrument = "AAPL",
+                        counterInstrument = "USD",
+                        price = BigDecimal(100),
+                        quantity = BigDecimal(3),
+                        acquisitionTime = 1,
+                        direction = Direction.LONG,
+                    ),
+                    Lot(
+                        instrument = "AAPL",
+                        counterInstrument = "USD",
+                        price = BigDecimal(200),
+                        quantity = BigDecimal(1),
+                        acquisitionTime = 2,
+                        direction = Direction.LONG,
+                    ),
+                )
+
+            val startingPortfolio =
+                Portfolio(
+                    positions = lots.toPositions(),
+                    cashPosition =
+                        CashPosition(
+                            instrument = "USD",
+                            quantity = BigDecimal(1_000),
+                        ),
+                )
+
+            val newTrade =
+                Trade(
+                    businessTime = 1,
+                    instrument = "AAPL",
+                    counterInstrument = "USD",
+                    settlementInstrument = "USD",
+                    price = BigDecimal(100),
+                    quantity = BigDecimal(5),
+                    type = TradeType.SELL,
+                    tradeSubType = TradeSubType.NONE,
+                    direction = Direction.LONG,
+                )
+
+            val resultingPortfolio = Accounting.addTrade(trade = newTrade, portfolio = startingPortfolio)
+
+            resultingPortfolio.cashPosition.quantity shouldBe BigDecimal(1_500)
+            resultingPortfolio.cashPosition.instrument shouldBe "USD"
+
+            val applPosition = resultingPortfolio.positions.filter { it.instrument == "AAPL" }[0]!!
+
+            applPosition.instrument shouldBe "AAPL"
+            applPosition.counterInstrument shouldBe "USD"
+            applPosition.price shouldBe BigDecimal(100)
+            applPosition.quantity shouldBe BigDecimal(-1)
+        }
     })
