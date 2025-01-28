d27931b54c102ad4c5b25300a71ed35f1f6e5a27
azody <zodyac@protonmail.com>
2025-01-28 14:47:21 -0500

    Testing Alpaca Market Data

--- src/main/kotlin/azody/atium/marketdata/Alpaca.kt
+++ src/main/kotlin/azody/atium/marketdata/Alpaca.kt
@@ -7,6 +7,8 @@ import kotlinx.serialization.json.Json
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import java.time.LocalDateTime
+import java.time.ZoneOffset
+import java.time.format.DateTimeFormatter
 
 @Serializable
 data class MarketDataResponse(
@@ -35,18 +37,16 @@ object AlpacaMarketData {
      * param: symbol
      * param: startDate in format YYYY-MM-DD or YYYY-MM-DDThh:mm:ss
      * param: endDate in format YYYY-MM-DD or YYYY-MM-DDThh:mm:ss
-     * param: timeFrame Alpaca Markets TImeframe
+     * param: timeFrame Alpaca Markets Timeframe
      */
     fun getOHLCData(
         symbol: String,
         startDate: String,
         endDate: String,
-        timeFrame: String,
+        timeframe: String,
     ): List<OHLC> {
         val apiKey = dotenv["ALPACA_API_KEY"] ?: throw IllegalStateException("ALPACA_API_KEY not set")
         val apiSecret = dotenv["ALPACA_SECRET_KEY"] ?: throw IllegalStateException("ALPACA_API_KEY not set")
-
-        val timeframe = "10Min"
 
         // Create an http client
         // Build the OkHttp client
@@ -73,9 +73,23 @@ object AlpacaMarketData {
         val data = response.body!!.string()
         val marketData = json.decodeFromString<MarketDataResponse>(data)
 
-        println(marketData)
         return marketData.bars[symbol]!!.toOHLC()
     }
 }
 
-fun List<BarData>.toOHLC(): List<OHLC> = listOf<OHLC>()
+fun List<BarData>.toOHLC(): List<OHLC> {
+    val formatter = DateTimeFormatter.ISO_DATE_TIME
+
+    return this.map {
+        val localDateTime = LocalDateTime.parse(it.t, formatter)
+        val epochMilli = localDateTime.toInstant(ZoneOffset.UTC).toEpochMilli()
+        OHLC(
+            businessTime = epochMilli,
+            open = it.o,
+            high = it.h,
+            low = it.l,
+            close = it.c,
+            volume = it.v.toDouble(),
+        )
+    }
+}
