44a40e1347eeb96cdc75b823225e8d8a2e2781f2
Andrew Zody <zodyac@protonmail.com>
2025-01-28 13:32:21 -0500

    Querying Alpaca Market Data

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,10 +12,9 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
 
-    implementation("io.ktor:ktor-client-core:3.0.3")
-    implementation("io.ktor:ktor-client-cio:3.0.3")
-    implementation("io.ktor:ktor-client-json:3.0.3")
-    implementation("io.ktor:ktor-client-content-negotiation:3.0.3")
+    implementation("com.squareup.okhttp3:okhttp:4.9.3")
+
+    implementation("io.github.cdimascio:dotenv-kotlin:6.4.0")
 
     testImplementation("io.kotest:kotest-runner-junit5:5.9.1")
     testImplementation("io.kotest:kotest-assertions-core:5.9.1")
--- src/main/kotlin/azody/atium/marketdata/Alpaca.kt
+++ src/main/kotlin/azody/atium/marketdata/Alpaca.kt
@@ -1,71 +1,67 @@
 package azody.atium.marketdata
 
-import io.ktor.client.*
-import io.ktor.client.call.*
-import io.ktor.client.engine.cio.*
-import io.ktor.client.plugins.*
-import io.ktor.client.plugins.contentnegotiation.*
-import io.ktor.client.request.*
-import io.ktor.client.statement.*
-import io.ktor.http.*
-import kotlinx.coroutines.runBlocking
+import io.github.cdimascio.dotenv.dotenv
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import java.time.LocalDateTime
 
 @Serializable
 data class MarketDataResponse(
-    val bars: List<Bar>,
+    val bars: Map<String, List<BarData>>,
 )
 
 @Serializable
-data class Bar(
-    val t: String, // Time
-    val o: Double, // Open
-    val h: Double, // High
-    val l: Double, // Low
-    val c: Double, // Close
+data class BarData(
+    val c: Double, // Close price
+    val h: Double, // High price
+    val l: Double, // Low price
+    val n: Int, // Number of trades
+    val o: Double, // Open price
+    val t: String, // Timestamp
     val v: Int, // Volume
-)
+    val vw: Double, // Volume-weighted average price
+) {
+    // Convert string timestamp to LocalDateTime
+    fun getTimestamp(): LocalDateTime = LocalDateTime.parse(t)
+}
 
-fun main() =
-    runBlocking {
-        val apiKey = ""
-        val apiSecret = ""
+fun main() {
+    val dotenv = dotenv()
 
-        val symbol = "PLTR"
-        val startDate = "2024-12-01T00:00:00Z" // ISO 8601 Format
-        val endDate = "2025-01-01T00:00:00Z"
-        val timeframe = "1Day"
+    val apiKey = dotenv["ALPACA_API_KEY"] ?: throw IllegalStateException("ALPACA_API_KEY not set")
+    val apiSecret = dotenv["ALPACA_SECRET_KEY"] ?: throw IllegalStateException("ALPACA_API_KEY not set")
 
-        // Create an http client
-        val client =
-            HttpClient(CIO) {
-                install(ContentNegotiation) {
-                    // json(Json { ignoreUnknownKeys = true })
-                }
-            }
+    val symbol = "PLTR"
+    val startDate = "2024-12-01" // ISO 8601 Format
+    val endDate = "2025-01-01"
+    val timeframe = "10Min"
 
-        try {
-            val response: HttpResponse =
-                client.get("https://paper-api.alpaca.markets/v2/stocks/$symbol/bars") {
-                    headers {
-                        append(HttpHeaders.Authorization, "Bearer $apiKey")
-                    }
-                    url {
-                        parameters.append("start", startDate)
-                        parameters.append("end", endDate)
-                        parameters.append("timeframe", timeframe)
-                    }
-                }
+    // Create an http client
+    // Build the OkHttp client
+    val client = OkHttpClient()
 
-            if (response.status.isSuccess()) {
-                val marketDataResponse: MarketDataResponse = response.body()
-                marketDataResponse.bars.forEach { bar ->
-                    println("Date: ${bar.t}, Open: ${bar.o}, High: ${bar.h}, Low: ${bar.l}, Close: ${bar.c}")
-                }
-            }
-        } catch (e: ClientRequestException) {
-            println(e)
-        } finally {
-            client.close()
+    val request =
+        Request
+            .Builder()
+            .url(
+                "https://data.alpaca.markets/v2/stocks/bars?limit=10000&symbols=$symbol&timeframe=$timeframe&start=$startDate&end=$endDate&adjustment=raw&feed=iex&sort=asc",
+            ).get()
+            .addHeader("accept", "application/json")
+            .addHeader("APCA-API-KEY-ID", "PK7FUUETM5D3QFDBWVZ2")
+            .addHeader("APCA-API-SECRET-KEY", "PaVnFSfDhez3UwBKlxfDb9CIx0zrfaHJ15acKr8A")
+            .build()
+
+    val response = client.newCall(request).execute()
+
+    val json =
+        Json {
+            ignoreUnknownKeys = true // This will ignore any unknown keys in the JSON
         }
-    }
+
+    val data = response.body!!.string()
+    val marketData = json.decodeFromString<MarketDataResponse>(data)
+
+    println(marketData)
+}
